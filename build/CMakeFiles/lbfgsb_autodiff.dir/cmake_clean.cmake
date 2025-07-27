893bc4e0e5720bf1372dce3b228749937615d122
Dwivedi07 <105978030+Dwivedi07@users.noreply.github.com>
2025-07-27 16:56:40 -0700

    Initial commit

---
+++ build/CMakeFiles/lbfgsb_autodiff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lbfgsb_autodiff.dir/main.cpp.o"
+  "lbfgsb_autodiff"
+  "lbfgsb_autodiff.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lbfgsb_autodiff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/quadratic_exec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadratic_exec.dir/quadratic.cpp.o"
+  "quadratic_exec"
+  "quadratic_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadratic_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
